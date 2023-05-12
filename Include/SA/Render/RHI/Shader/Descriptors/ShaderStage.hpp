9ddaa8d259baed50bb9c32fd7abbed3c6c5ea7ef
mrouffet <maximerouffet@gmail.com>
2023-05-13 08:52:42 +0900

    [Add] RHI:: ShaderStage

---
+++ Include/SA/Render/RHI/Shader/Descriptors/ShaderStage.hpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_RENDER_SHADER_STAGE_GUARD
+#define SAPPHIRE_RENDER_SHADER_STAGE_GUARD
+
+#include <cstdint>
+
+#if SA_RENDER_LOWLEVEL_VULKAN_IMPL
+
+	#include <SA/Render/LowLevel/Vulkan/VulkanAPI.hpp>
+
+#endif
+
+namespace SA::RND
+{
+	namespace RHI
+	{
+		enum ShaderStage : uint8_t
+		{
+			Unknown = 0,
+
+			Vertex = 1 << 0,
+
+			Pixel = 1 << 1,
+			Fragment = Pixel,
+
+			Geometry = 1 << 2,
+
+			Compute = 1 << 3,
+		};
+	}
+
+#if SA_RENDER_LOWLEVEL_VULKAN_IMPL
+
+	namespace VK
+	{
+		VkShaderStageFlagBits API_GetShaderStage(RHI::ShaderStage _type) noexcept;
+
+		VkShaderStageFlags API_GetShaderStageFlags(RHI::ShaderStage _flags) noexcept;
+	}
+
+#endif
+}
+
+#endif // SAPPHIRE_RENDER_SHADER_STAGE_GUARD
---
+++ Source/SA/Render/RHI/Shader/Descriptors/ShaderStage.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
+
+#include <Shader/Descriptors/ShaderStage.hpp>
+
+namespace SA::RND
+{
+#if SA_RENDER_LOWLEVEL_VULKAN_IMPL
+
+	namespace VK
+	{
+		VkShaderStageFlagBits API_GetShaderStage(RHI::ShaderStage _type) noexcept
+		{
+			switch (_type)
+			{
+			case RHI::ShaderStage::Vertex:
+				return VK_SHADER_STAGE_VERTEX_BIT;
+			case RHI::ShaderStage::Fragment:
+				return VK_SHADER_STAGE_FRAGMENT_BIT;
+			case RHI::ShaderStage::Geometry:
+				return VK_SHADER_STAGE_GEOMETRY_BIT;
+			case RHI::ShaderStage::Compute:
+				return VK_SHADER_STAGE_COMPUTE_BIT;
+			case RHI::ShaderStage::Unknown:
+			default:
+				SA_LOG(L"ShaderStage [%1] not supported yet!", Error, SA.Render.RHI.Vulkan);
+				return VkShaderStageFlagBits(0);
+			}
+		}
+
+		VkShaderStageFlags API_GetShaderStageFlags(RHI::ShaderStage _flags) noexcept
+		{
+			VkShaderStageFlags result = 0;
+
+			if (_flags & RHI::ShaderStage::Vertex)
+				result |= VK_SHADER_STAGE_VERTEX_BIT;
+
+			if (_flags & RHI::ShaderStage::Fragment)
+				result |= VK_SHADER_STAGE_FRAGMENT_BIT;
+
+			if (_flags & RHI::ShaderStage::Geometry)
+				result |= VK_SHADER_STAGE_GEOMETRY_BIT;
+
+			if (_flags & RHI::ShaderStage::Compute)
+				result |= VK_SHADER_STAGE_COMPUTE_BIT;
+
+			return result;
+		}
+	}
+
+#endif
+}
