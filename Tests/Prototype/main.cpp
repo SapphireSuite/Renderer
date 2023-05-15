179308261728f46fe73444b6e8d3872b317a1f6b
mrouffet <maximerouffet@gmail.com>
2023-05-15 19:02:29 +0900

    [Add] RHI sample

--- Tests/Prototype/main.cpp
+++ Tests/Prototype/main.cpp
@@ -2,13 +2,34 @@
 
 #include <SA/Collections/Debug>
 
+#include <SA/Render/RHI/RHIVkRenderInterface.hpp>
+#include <SA/Render/RHI/RHID12RenderInterface.hpp>
+using namespace SA::RND;
+
+RHI::RenderInterface* intf;
+RHI::Device* device;
+
 void Init()
 {
 	SA::Debug::InitDefaultLogger();
+
+	intf = new RHI::VkRenderInterface();
+	// intf = new RHI::D12RenderInterface();
+
+	intf->Create();
+
+	// Device
+	{
+		auto infos = intf->QueryDeviceInfos();
+
+		device = intf->CreateDevice(infos[0].get());
+	}
 }
 
 void Uninit()
 {
+	intf->Destroy();
+	delete intf;
 }
 
 int main()
