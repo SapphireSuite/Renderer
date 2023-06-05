e134517c6ecea1edd9289de7d7899419fc367f93
Maxime ROUFFET <rmaxime@square-enix.com>
2023-06-05 17:06:58 +0900

    [Add] RHI Shader

---
+++ Source/SA/Render/RHI/Shader/RHID12Shader.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
+
+#include <Shader/RHID12Shader.hpp>
+
+#if SA_RENDER_LOWLEVEL_DX12_IMPL
+
+#include <SA/Render/ShaderCompiler/ShaderCompileResult.hpp>
+
+namespace SA::RND::RHI
+{
+	void D12Shader::Create(const Device& _device, const ShaderCompileResult& _compil)
+	{
+		Shader::Create(_device, _compil);
+
+		mHandle.handle = reinterpret_cast<const MComPtr<ID3DBlob>&>(_compil.dxShader);
+	}
+	
+	void D12Shader::Destroy(const Device& _device)
+	{
+		(void)_device;
+
+		mHandle.handle = nullptr;
+	}
+
+	
+	const DX12::Shader& D12Shader::API_DirectX12() const
+	{
+		return mHandle;
+	}
+}
+
+#endif // SA_RENDER_LOWLEVEL_DX12_IMPL
