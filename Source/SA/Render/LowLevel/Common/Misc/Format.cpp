7f2e094010543161ef80f0c665bfc62e2419d6b7
Maxime ROUFFET <maximerouffet@gmail.com>
2024-03-02 09:05:41 +0900

    [Rework] Format in LowLevel

---
+++ Source/SA/Render/LowLevel/Common/Misc/Format.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
+
+#include <Misc/Format.hpp>
+
+namespace SA::RND
+{
+	uint32_t API_GetChannelNum(Format _format)
+	{
+		uint32_t uiFormat = static_cast<uint32_t>(_format);
+
+		if ((uiFormat >= static_cast<uint32_t>(Format::R8_UNORM) && uiFormat <= static_cast<uint32_t>(Format::R8_SINT)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R16_UNORM) && uiFormat <= static_cast<uint32_t>(Format::R16_SFLOAT)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R32_UINT) && uiFormat <= static_cast<uint32_t>(Format::R32_SFLOAT)) ||
+			(_format == Format::D16_UNORM || _format == Format::D32_SFLOAT))
+			return 1u;
+		
+		if ((uiFormat >= static_cast<uint32_t>(Format::R8G8_UNORM) && uiFormat <= static_cast<uint32_t>(Format::R8G8_SINT)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R16G16_UNORM) && uiFormat <= static_cast<uint32_t>(Format::R16G16_SFLOAT)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R32G32_UINT) && uiFormat <= static_cast<uint32_t>(Format::R32G32_SFLOAT)) ||
+			(_format == Format::D24_UNORM_S8_UINT))
+			return 2u;
+		
+		if (uiFormat >= static_cast<uint32_t>(Format::R32G32B32_UINT) && uiFormat <= static_cast<uint32_t>(Format::R32G32B32_SFLOAT))
+			return 3u;
+		
+		if ((uiFormat >= static_cast<uint32_t>(Format::R8G8B8A8_UNORM) && uiFormat <= static_cast<uint32_t>(Format::B8G8R8A8_SRGB)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R16G16B16A16_UNORM) && uiFormat <= static_cast<uint32_t>(Format::R16G16B16A16_SFLOAT)) ||
+			(uiFormat >= static_cast<uint32_t>(Format::R32G32B32A32_UINT) && uiFormat <= static_cast<uint32_t>(Format::R32G32B32A32_SFLOAT)))
+			return 4u;
+	}
+}
---
+++ Source/SA/Render/LowLevel/Vulkan/Misc/VkFormat.cpp
@@ -0,0 +1,93 @@
+// Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
+
+#include <SA/Render/LowLevel/Common/Misc/Format.hpp>
+
+namespace SA::RND::VK
+{
+	static const VkFormat vkFormatIndexMap[] = {
+		VK_FORMAT_UNDEFINED,
+
+		VK_FORMAT_R8_UNORM,
+		VK_FORMAT_R8_SNORM,
+		VK_FORMAT_R8_UINT,
+		VK_FORMAT_R8_SINT,
+
+		VK_FORMAT_R8G8_UNORM,
+		VK_FORMAT_R8G8_SNORM,
+		VK_FORMAT_R8G8_UINT,
+		VK_FORMAT_R8G8_SINT,
+
+		VK_FORMAT_R8G8B8A8_UNORM,
+		VK_FORMAT_R8G8B8A8_SNORM,
+		VK_FORMAT_R8G8B8A8_UINT,
+		VK_FORMAT_R8G8B8A8_SINT,
+		VK_FORMAT_R8G8B8A8_SRGB,
+
+		VK_FORMAT_B8G8R8A8_UNORM,
+		VK_FORMAT_B8G8R8A8_SNORM,
+		VK_FORMAT_B8G8R8A8_UINT,
+		VK_FORMAT_B8G8R8A8_SINT,
+		VK_FORMAT_B8G8R8A8_SRGB,
+
+		VK_FORMAT_R16_UNORM,
+		VK_FORMAT_R16_SNORM,
+		VK_FORMAT_R16_UINT,
+		VK_FORMAT_R16_SINT,
+		VK_FORMAT_R16_SFLOAT,
+
+		VK_FORMAT_R16G16_UNORM,
+		VK_FORMAT_R16G16_SNORM,
+		VK_FORMAT_R16G16_UINT,
+		VK_FORMAT_R16G16_SINT,
+		VK_FORMAT_R16G16_SFLOAT,
+
+		VK_FORMAT_R16G16B16A16_UNORM,
+		VK_FORMAT_R16G16B16A16_SNORM,
+		VK_FORMAT_R16G16B16A16_UINT,
+		VK_FORMAT_R16G16B16A16_SINT,
+		VK_FORMAT_R16G16B16A16_SFLOAT,
+
+		VK_FORMAT_R32_UINT,
+		VK_FORMAT_R32_SINT,
+		VK_FORMAT_R32_SFLOAT,
+
+		VK_FORMAT_R32G32_UINT,
+		VK_FORMAT_R32G32_SINT,
+		VK_FORMAT_R32G32_SFLOAT,
+
+		VK_FORMAT_R32G32B32_UINT,
+		VK_FORMAT_R32G32B32_SINT,
+		VK_FORMAT_R32G32B32_SFLOAT,
+
+		VK_FORMAT_R32G32B32A32_UINT,
+		VK_FORMAT_R32G32B32A32_SINT,
+		VK_FORMAT_R32G32B32A32_SFLOAT,
+
+		VK_FORMAT_D16_UNORM,
+		VK_FORMAT_D32_SFLOAT,
+		VK_FORMAT_D24_UNORM_S8_UINT,
+	};
+
+	VkFormat API_GetFormat(Format _format)
+	{
+		const uint32_t index = static_cast<uint32_t>(_format);
+
+		SA_ASSERT((OutOfRange, index, 0u, sizeof(vkFormatIndexMap)), SA.Render.RHI.Vulkan,
+			(L"Format value [%1] invalid", index));
+
+		return vkFormatIndexMap[index];
+	}
+
+	Format API_GetFormat(VkFormat _vkFormat)
+	{
+		for(uint32_t i = 0; i < sizeof(vkFormatIndexMap); ++i)
+		{
+			if(vkFormatIndexMap[i] == _vkFormat)
+				return static_cast<Format>(i);
+		}
+
+		SA_LOG((L"VkFormat [%1] not supported yet!", static_cast<uint32_t>(_vkFormat)), Error, Sa.Render.RHI.Vulkan);
+
+		return Format::R8G8B8A8_UNORM;
+	}
+}
