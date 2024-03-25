0b5a3a3e61cc90b950cff09053fe4a17c46fd070
Maxime ROUFFET <maximerouffet@gmail.com>
2024-03-25 19:14:44 +0900

    [Add] VK:: RenderPass

---
+++ Include/SA/Render/LowLevel/Vulkan/Pass/VkRenderPass.hpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2024 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_RENDER_VK_RENDER_PASS_GUARD
+#define SAPPHIRE_RENDER_VK_RENDER_PASS_GUARD
+
+#include "Info/VkRenderPassInfo.hpp"
+
+namespace SA::RND::VK
+{
+	class Device;
+	class CommandBuffer;
+	class FrameBuffer;
+
+	class RenderPass
+	{
+		VkRenderPass mHandle = VK_NULL_HANDLE;
+
+	public:
+		void Create(const Device& _device, const RenderPassInfo& _info);
+		void Create(const Device& _device, const VkRenderPassCreateInfo& _vkInfo);
+		void Destroy(const Device& _device);
+
+		void Begin(const CommandBuffer& _cmd, const FrameBuffer& _fBuff);
+		void NextSubpass(const CommandBuffer& _cmd);
+		void End(const CommandBuffer& _cmd);
+
+		operator VkRenderPass() const noexcept;
+	};
+}
+
+#endif // SAPPHIRE_RENDER_VK_RENDER_PASS_GUARD
