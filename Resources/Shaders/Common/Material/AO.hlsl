6c045cc0ab3373e9bdb15628e772bf2f833d311f
Maxime ROUFFET <rmaxime@square-enix.com>
2024-04-08 09:28:20 +0900

    [Add] Material AO.hlsl

---
+++ Resources/Shaders/Common/Material/AO.hlsl
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Sapphire's Suite. All Rights Reserved.
+
+#ifndef SAPPHIRE_RENDER_SHADER_AO_GUARD
+#define SAPPHIRE_RENDER_SHADER_AO_GUARD
+
+namespace SA
+{
+#if SA_MATERIAL_AO_ID
+	
+	#define SA_MATERIAL_AO 1
+	
+	Texture2D<float> ao : SA_REG_SPACE(t, SA_MATERIAL_AO_ID, 3);
+	SamplerState aoSampler : SA_REG_SPACE(s, SA_MATERIAL_AO_ID, 3);
+
+#else // SA_MATERIAL_AO_ID
+	
+	#define SA_MATERIAL_AO 0
+	
+#endif  // SA_MATERIAL_AO_ID
+	
+	
+	//---------- Helper Functions ----------
+
+	float SampleAO(float2 _uv)
+	{
+#if SA_MATERIAL_AO
+			
+		return ao.Sample(aoSampler, _uv);
+			
+#else
+		// Default ao value.
+		return 1.0f;
+
+#endif
+	}
+}
+
+#endif // SAPPHIRE_RENDER_SHADER_AO_GUARD
