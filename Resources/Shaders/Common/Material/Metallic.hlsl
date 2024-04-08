0bddddd3c5729659dc76e6ea049e09154251d87f
Maxime ROUFFET <rmaxime@square-enix.com>
2024-04-08 09:24:49 +0900

    [Add] Material Metallic.hlsl

---
+++ Resources/Shaders/Common/Material/Metallic.hlsl
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Sapphire's Suite. All Rights Reserved.
+
+#ifndef SAPPHIRE_RENDER_SHADER_METALLIC_GUARD
+#define SAPPHIRE_RENDER_SHADER_METALLIC_GUARD
+
+namespace SA
+{
+#if SA_MATERIAL_METALLIC_ID
+	
+	#define SA_MATERIAL_METALLIC 1
+	
+	Texture2D<float> metallic : SA_REG_SPACE(t, SA_MATERIAL_METALLIC_ID, 3);
+	SamplerState metallicSampler : SA_REG_SPACE(s, SA_MATERIAL_METALLIC_ID, 3);
+
+#else // SA_MATERIAL_METALLIC_ID
+	
+	#define SA_MATERIAL_METALLIC 0
+	
+#endif  // SA_MATERIAL_METALLIC_ID
+	
+	
+	//---------- Helper Functions ----------
+
+	float SampleMetallic(float2 _uv)
+	{
+#if SA_MATERIAL_METALLIC
+			
+		return metallic.Sample(metallicSampler, _uv);
+			
+#else
+		// Default metallic value.
+		return 0.5f;
+			
+#endif
+	}
+}
+
+#endif // SAPPHIRE_RENDER_SHADER_METALLIC_GUARD
