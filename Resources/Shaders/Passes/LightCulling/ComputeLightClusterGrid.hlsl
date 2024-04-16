618f81362a9e2fe02b21bf9b2218c86d4adfe1ef
Maxime ROUFFET <rmaxime@square-enix.com>
2024-04-16 12:39:45 +0900

    [Fix] Naming "View" instead of "World"

--- Resources/Shaders/Passes/LightCulling/ComputeLightClusterGrid.hlsl
+++ Resources/Shaders/Passes/LightCulling/ComputeLightClusterGrid.hlsl
@@ -33,7 +33,7 @@ RWStructuredBuffer<ClusterAABB> clusterAABBs : register(u0);
 
 //-------------------- Compute Shader --------------------
 
-float3 ComputeScreenSpaceToWorldSpace(float2 _ssPos);
+float3 ComputeScreenSpaceToViewSpace(float2 _ssPos);
 float3 LineIntersectionWithZPlane(/*float3 _start, */float3 _end, float _zPlane);
 
 [numthreads(NUM_THREAD_X, NUM_THREAD_Y, NUM_THREAD_Z)]
@@ -51,11 +51,11 @@ void main(uint3 DispatchThreadID : SV_DispatchThreadID)
 	const float2 ssMin = float2(DispatchThreadID.x, DispatchThreadID.y) * tilingSize;
 	const float2 ssMax = float2(DispatchThreadID.x + 1, DispatchThreadID.y + 1) * tilingSize;
 	
-	// Compute World-Space min, max.
-	const float3 wsMin = ComputeScreenSpaceToWorldSpace(ssMin);
-	const float3 wsMax = ComputeScreenSpaceToWorldSpace(ssMax);
+	// Compute View-Space min, max.
+	const float3 wsMin = ComputeScreenSpaceToViewSpace(ssMin);
+	const float3 wsMax = ComputeScreenSpaceToViewSpace(ssMax);
 
-	// Compute World-Space cluster near, far.
+	// Compute View-Space cluster near, far.
 	const float clusterNear = camera.zNear * pow(camera.zFar / camera.zNear, DispatchThreadID.z / clusterGridSize.z);
 	const float clusterFar = camera.zNear * pow(camera.zFar / camera.zNear, (DispatchThreadID.z + 1) / clusterGridSize.z);
 	
@@ -69,7 +69,7 @@ void main(uint3 DispatchThreadID : SV_DispatchThreadID)
 	clusterAABBs[clusterIndex].max = clusterMaxAABB;
 }
 
-float3 ComputeScreenSpaceToWorldSpace(float2 _ssPos)
+float3 ComputeScreenSpaceToViewSpace(float2 _ssPos)
 {
 	// Convert to NDC
 	const float2 texCoord = _ssPos / camera.screen.xy;
@@ -77,11 +77,11 @@ float3 ComputeScreenSpaceToWorldSpace(float2 _ssPos)
 	// Convert to clip space
 	const float4 clipPos = float4(texCoord * 2.0 - 1.0, 0.0, 1.0);
 	
-	// Convert to world space
-	const float4 worldPos = mul(clipPos, camera.inverseProjection);
+	// Convert to view-space
+	const float4 viewPos = mul(clipPos, camera.inverseProjection);
 	
 	// Fix perspective
-	return worldPos.xyz / worldPos.w;
+	return viewPos.xyz / viewPos.w;
 }
 
 // Simplification as _start is always float3(0, 0, 0) (camera origin).
