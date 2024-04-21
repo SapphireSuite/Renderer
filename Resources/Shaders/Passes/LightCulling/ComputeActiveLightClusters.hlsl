e1873939606a24c59a1debb5217b5c143607ca24
Maxime ROUFFET <maximerouffet@gmail.com>
2024-04-21 16:04:05 +0900

    [Fix] View-Space depth reconstruction

--- Resources/Shaders/Passes/LightCulling/ComputeActiveLightClusters.hlsl
+++ Resources/Shaders/Passes/LightCulling/ComputeActiveLightClusters.hlsl
@@ -30,22 +30,28 @@ void main(uint3 _dispatchThreadID : SV_DispatchThreadID)
 		_dispatchThreadID.y >= depthTextureSize.y)
 		return;
 	
-	const float depthValue = depthTexture[_dispatchThreadID.xy];
+	const float sampledDepth = depthTexture[_dispatchThreadID.xy];
 	
 	// Discard empty depth.
 #if SA_DEPTH_INVERTED
 
-	if(depthValue == 0.0f)
+	if(sampledDepth == 0.0f)
 		return;
 
 #else
 	
-	if (depthValue == 1.0f)
+	if (sampledDepth == 1.0f)
 		return;
 	
 #endif
 
-	const uint clusterIndex = SA::GetClusterIndex(_dispatchThreadID.xy, depthValue);
+	// Convert to view-space depth position.
+	const float4 viewDepthPosition = mul(camera.inverseProjection, float4(0.0f, 0.0f, sampledDepth, 1.0f));
+	
+	// Fix perspective.
+	const float vsDepth = viewDepthPosition.z / viewDepthPosition.w;
+
+	const uint clusterIndex = SA::GetClusterIndex(_dispatchThreadID.xy, vsDepth);
 	
 	activeClusterStates[clusterIndex] = true;
 }
